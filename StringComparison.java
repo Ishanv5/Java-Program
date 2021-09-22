4690f8d26c8f8abadf337b9022c759f285317651
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2021-09-22 11:07:01 +0530

    Add files via upload

---
+++ StringComparison.java
@@ -0,0 +1,47 @@
+import javax.lang.model.util.ElementScanner14;
+
+// String Comparison in java
+class StringComparison {
+    public static void main(String[] args) {
+      /*  String a=" Lion ";
+        String b=" Lion ";
+        if(a==b ) // equal operator
+        {
+          System.out.print(" True ");
+        }
+        else
+        {
+            System.out.print(" False ");
+        }  */
+      /* String a=" Lion "; // String literal
+        String b=" LION ";
+        if(a==b ) // equal operator
+        {
+          System.out.print(" True ");
+        }
+        else
+        {
+            System.out.print(" False ");
+        } */
+      /*  String a=" Lion "; // String literal
+        String b=" Tiger";    if(a==b ) // equal operator
+        {
+          System.out.print(" True ");
+        }
+        else
+        {
+            System.out.print(" False ");
+        } */
+        String a=" Lion "; // String literal
+        String b=new String(" Tiger");  // new  Keyword
+          if(a==b ) // equal operator
+        {
+          System.out.print(" True ");
+        }
+        else
+        {
+            System.out.print(" False ");
+        } 
+        
+    }
+}
