50e965f5d4847b422591aafbbaa6154f829eda2c
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2021-09-23 11:44:36 +0530

    Create Con2.java

---
+++ Con2.java
@@ -0,0 +1,23 @@
+// Java Constructor 
+public class Constructor {
+ int a;
+ String name;
+ Constructor()
+ {
+	 a=0;
+	 name=null;
+ }
+ void show()
+ {
+	 Systemiout.print(a+" "name);
+ }
+}
+class Con2
+{
+	public static void main(String[]args)
+	{
+		Constructor ref=new Constructor();
+		r.show();
+	}
+	
+}
