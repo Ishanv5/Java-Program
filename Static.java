68817a810989c693fbcb5ebb285a07e7ad54e228
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2021-09-25 18:43:25 +0530

    Add files via upload

---
+++ Static.java
@@ -0,0 +1,18 @@
+// Static block
+class Static{
+    static
+    {
+        System.out.println(" Learn code with Ishan !... ");
+    }
+    Static()
+    {
+        System.out.println(" Progress your coding skill with Ishan "); 
+    }
+    {
+        System.out.println(" Code with Ishan ");
+    }
+    public static void main(String[] args) {
+        Static r=new Static();
+
+    }
+}
