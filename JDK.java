e661c86e36f5455a5c46a25472fd0c9c30ad3445
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2021-10-02 19:06:47 +0530

    Add files via upload

--- JDK.java
+++ JDK.java
@@ -3,6 +3,10 @@ interface A
 {
     void a1();    
     void a2(); 
+    default void a3()
+    {
+        System.out.println(" May or may not override in implementing class ");
+    }
 }
 class B implements A 
 {
@@ -12,7 +16,7 @@ class B implements A
     }
     public void a2()
     {
-        System.out.print(" Class B a2() ");
+        System.out.println(" Class B a2() ");
     }
 }
 class C implements A 
@@ -23,7 +27,7 @@ class C implements A
     }
     public void a2()
     {
-        System.out.print(" Class C a2() ");
+        System.out.println(" Class C a2() ");
     }
 }
 class D implements A 
@@ -34,18 +38,18 @@ class D implements A
     }
     public void a2()
     {
-        System.out.print(" Class D a2() ");
+        System.out.println(" Class D a2() ");
     }
 }
 class JDK
 {
     public static void main(String[] args) {
         B b=new B();
-        b.a1(); b.a2();
+        b.a1(); b.a2();b.a3();
         C c=new C();
-        c.a1(); c.a2();
+        c.a1(); c.a2();c.a3();
         D d=new D();
-        d.a1(); d.a2();
+        d.a1(); d.a2();d.a3();
 
     }
 }
