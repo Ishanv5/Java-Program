1d1defad83fad19ed1048446d8b16510552bb921
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2021-10-07 15:02:48 +0530

    Add files via upload

---
+++ throwDemo.java
@@ -0,0 +1,9 @@
+// throw keyword 
+class throwDemo
+{
+    public static void main(String[] args) {
+       System.out.println(10/2);
+      throw new ArithmeticException(" Divide by zero ");
+
+    }
+}
