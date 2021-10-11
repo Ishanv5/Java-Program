2d57c2bcb7e76c6f901a2bf4fd508e77c8425087
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2021-10-11 15:50:41 +0530

    Add files via upload

---
+++ pack.java
@@ -0,0 +1,13 @@
+// User defined package 
+package ishan;
+class pack
+{
+    private void show()
+    {
+        System.out.println(" Learn code ");
+    }
+    public static void main(String[] args) {
+        pack r=new pack();
+        r.show();
+    }
+}
