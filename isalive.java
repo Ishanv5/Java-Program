7660aee4e447305a2445ac81f7e2d0ff3d406639
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2021-10-15 11:07:42 +0530

    Add files via upload

---
+++ isalive.java
@@ -0,0 +1,20 @@
+// isAlive method 
+class A extends Thread
+{
+    public void run()
+    {
+        System.out.println(" isAlive method program ... ");
+    }
+}
+class isalive 
+{
+    public static void main(String[] args) {
+        A t1=new A();
+        A t2=new A();
+        System.out.println(t1.isAlive());
+        t1.start();
+        
+        System.out.println(t1.isAlive());
+        t2.start();
+    }
+}
